cbb5a84a648c379174cd6138b700b165a106dcdd
Shayan Talebian <shayantalebianwork@gmail.com>
2025-02-24 17:29:43 +0330

    move snake logic to separate Snake class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle as Turtle_Module
+from snake import Snake
 import time as Time
 
 screen = Turtle_Module.Screen()
@@ -7,28 +8,12 @@ screen.bgcolor("black")
 screen.title("Snake Game 🐍 | Shayan Talebian 👨🏻‍💻")
 screen.tracer(0)
 
-
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle_Module.Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
     
 game_is_on = True
 while game_is_on:
     screen.update()
     Time.sleep(0.1)
-    
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    
-    segments[0].forward(20)
+    snake .move()
     
 screen.exitonclick()
